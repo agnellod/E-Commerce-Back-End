39bbd2a62bf4c2ebb891e6588ece4a6d677275ef
Dylan <agnello.dylan95@gmail.com>
2023-03-29 21:23:01 -0600

    fixing tags create function

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -15,11 +15,11 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      references: {model: 'product', key: 'id', unique: false}
+      references: {model: 'product', key: 'id'}
     },
     tag_id: { 
       type: DataTypes.INTEGER,
-      references: {model: 'tag', key: 'id', unique: false}
+      references: {model: 'tag', key: 'id'}
     },
   },
   {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const tagData = await Tag.create({tag_name: req.body.tag_name});
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
